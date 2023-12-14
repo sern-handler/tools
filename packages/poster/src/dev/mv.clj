563e0e0eda776a469bd05c7eedda2b44d0851fbe
Jacob Nguyen <76754747+jacoobes@users.noreply.github.com>
2023-12-13 19:03:37 -0600

    fix if not exists dist

--- packages/poster/src/dev/mv.clj
+++ packages/poster/src/dev/mv.clj
@@ -1,8 +1,10 @@
 (ns dev.mv)
-
+(require '[clojure.java.io :as io])
 (defn hook 
   {:shadow.build/stage :compile-finish}
   [build-state & args]
-  (do (spit "./dist/discord.d.ts" (slurp "./dts/discord.d.ts"))
+  (do 
+      (when (not (.exists (io/file "./dist")) (.mkdir (io/file "./dist"))))
+      (spit "./dist/discord.d.ts" (slurp "./dts/discord.d.ts"))
       (spit "./dist/index.d.ts" (slurp "./dts/index.d.ts")))
   build-state)

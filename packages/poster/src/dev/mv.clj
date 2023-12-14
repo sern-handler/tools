510b6e264879fa7ff8f3c93acac8b1108f619df8
Jacob Nguyen <76754747+jacoobes@users.noreply.github.com>
2023-12-13 19:06:29 -0600

    oops

--- packages/poster/src/dev/mv.clj
+++ packages/poster/src/dev/mv.clj
@@ -4,7 +4,7 @@
   {:shadow.build/stage :compile-finish}
   [build-state & args]
   (do 
-      (when (not (.exists (io/file "./dist")) (.mkdir (io/file "./dist"))))
+      (when (not (.exists (io/file "./dist"))) (.mkdir (io/file "./dist")))
       (spit "./dist/discord.d.ts" (slurp "./dts/discord.d.ts"))
       (spit "./dist/index.d.ts" (slurp "./dts/index.d.ts")))
   build-state)

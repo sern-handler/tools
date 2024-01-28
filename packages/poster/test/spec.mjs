4cf38278737fa0b2b7e5b1e1a086586df8fe3f6b
jacob <jacoobes@sern.dev>
2024-01-28 17:44:24 -0600

    oops regen token

--- packages/poster/test/spec.mjs
+++ packages/poster/test/spec.mjs
@@ -1,6 +1,6 @@
 import poster from '../dist/index.js';
 
-const send = await poster.client("MTA2MTQyMTgzNDM0MTQ2MjAzNg.GL15Ob.EJ9SgdMQUhTTTGRhRKNBvVDQkTma7AG3DuLWis");
+const send = await poster.client("");
 
 const req = await send("global/get-all", { 
     
